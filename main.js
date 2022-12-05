a281334e361ed6bd5f4cee8255943cbfaf20267a
Ron543321s <96761305+Ron543321s@users.noreply.github.com>
2022-12-05 18:10:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function prelode() {
+    
+}
+
+function setup() {
+ canvas = createCanvas(640, 490);
+ canvas.position(110, 250);
+ video = createCapture(VIDEO);
+ video.hide();
+ 
+ tint_color ="";
+}
+
+function draw() {
+    image(video,0,0,640,490);
+    tint(tint_color);
+}
+
+function take_snapshop() {
+    save('student_name.png');
+}
+
+function applytint() {
+    tint_color = document.getElementById("color_name").value;
+}
